6eca7edd64d0ba2d856eddbd511982883495cd85
przemyslaw.luczak <przemyslaw.luczak.92@gmail.com>
2020-11-29 17:21:53 +0100

    initial commit

---
+++ src/main/java/pl/luczak/przemyslaw/pracadomowatydzien9/PracaDomowaTydzien9Application.java
@@ -0,0 +1,13 @@
+package pl.luczak.przemyslaw.pracadomowatydzien9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaDomowaTydzien9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracaDomowaTydzien9Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/luczak/przemyslaw/pracadomowatydzien9/PracaDomowaTydzien9ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.luczak.przemyslaw.pracadomowatydzien9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracaDomowaTydzien9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
